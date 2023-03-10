131c0a2a2450df12598234d0f73737f424f8eba3
Krishna <krishnarmakwana7312@gmail.com>
2023-03-10 23:33:17 +0530

    Adding project files to repository

--- src/main/java/com/imageoperationtask/ImageOperationTaskApplication.java
+++ src/main/java/com/imageoperationtask/ImageOperationTaskApplication.java
@@ -2,10 +2,8 @@ package com.imageoperationtask;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
 @SpringBootApplication
 public class ImageOperationTaskApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(ImageOperationTaskApplication.class, args);
 	}
